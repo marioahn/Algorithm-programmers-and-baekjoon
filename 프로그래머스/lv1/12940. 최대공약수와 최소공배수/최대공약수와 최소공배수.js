5bb73d6ec9f88e6418317eae2ba790de4d7b6401
marioahn <89332401+marioahn@users.noreply.github.com>
2023-06-23 23:23:19 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,22 +1,20 @@
-// 두 수 m과 n이 있을 때, m * n = 최대공약수(GCD) * 최소공배수(LCM)
-  // 최대공약수 구하는게 더 빠를거 같다
-  // m,n은 1이상 1000000이하의 자연수 -> 유클리드 호제법으로 구하자
-
-// 유클리드호제법: big > small인 경우 -> 중간에 small리턴하는거임 ㅇㅇ주의!!
-function gcd(big,small) {
-  let remainder = big % small
-  if (remainder===0) return small
-  return gcd(small,remainder)
-}
-
 function solution(n, m) {
-  let gcdNum = 0
-  if (n>=m) {
-    gcdNum = gcd(n,m)
-  } else {
-    gcdNum = gcd(m,n)
-  }
+    // 최대공약수를 구하는 함수
+    function gcd(a, b) {
+        while (b !== 0) { // b가 0이 아닐 때 까지 돌아라 이하는 swap 한 것!! 
+            let c;
+            c = b;
+            b = a % b;
+            a = c;
+        }
+        return a;
+    }
 
-  return [gcdNum, n*m/gcdNum]
+    // 최소공배수를 구하는 함수
+    function lcm(a, b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    // 최대공약수와 최소공배수를 배열로 반환
+    return [gcd(n, m), lcm(n, m)];
 }
-
