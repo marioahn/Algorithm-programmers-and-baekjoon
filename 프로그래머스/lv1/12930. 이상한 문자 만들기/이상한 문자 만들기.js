c79306e745bb3a5593f5fc21579a2fc52d816509
marioahn <89332401+marioahn@users.noreply.github.com>
2023-07-06 01:01:25 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,3 +1,7 @@
+// 하나 이.상의 공백문자로 구분 (공백 두번도 있을 수 있는건가?) -> 아 그게아니라, 여러 단어있단뜻
+  // split으로 각 단어가 요소로 들어간 배열 새로 만들고,
+  // 거기서 각 요소들의 인덱스를 또 들어가서(이중포문) 대/소문자화 하는거 밖에 생각이 안나..
+
 function solution(s) {
   var answer = '';
   let sArr = s.split(" ")
@@ -12,3 +16,5 @@ function solution(s) {
 
   return answer;
 }
+
+console.log(solution("try hello world"))
