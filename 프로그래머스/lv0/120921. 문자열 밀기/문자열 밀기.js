3e1f0959dfab80ef7476e40713b167b14a8a4dc1
marioahn <89332401+marioahn@users.noreply.github.com>
2023-07-06 00:20:25 +0900

    [level 0] Title: 문자열 밀기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,6 +1,8 @@
 function solution(A, B) {
+  // step1
   if (A===B) return 0
 
+  // step2
   let cnt = 0
   let tmp = A // 초기값 담아주고
   for (let i=0; i<A.length-1; i++) {
@@ -10,7 +12,6 @@ function solution(A, B) {
     if (moveA === B) return cnt // break
   }
 
-  if (cnt === A.length-1) return -1
+  // step3 -> step2에서 return이 안 나온다 => A는 B가 될 수 없다
+  return -1
 }
-
-// console.log(solution("hello","ohell"))
