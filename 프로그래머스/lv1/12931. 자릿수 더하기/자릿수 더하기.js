980dc064e05187194e9bfb0dd47ad99830406789
marioahn <89332401+marioahn@users.noreply.github.com>
2023-07-08 00:04:18 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,10 +1,16 @@
 function solution(n) { // n = 123
-  let answer = 0;
-  let tmp = String(n) // tmp = '123'
+    let answer = 0;
+    let tmp = String(n) // tmp = '123'
 
-  for (let i=0; i<tmp.length; i++) {
-    answer += Number(tmp[i])
-  }
+    for (let i=0; i<tmp.length; i++) {
+        answer += Number(tmp[i])
+    }
 
-  return answer;
+    return answer;
 }
+
+// 다른 코드 
+    //쉬운방법 -> let b = 123 + "" // ㅅㅂ.. 자동 형변환경우.. (=String(n)과 동일)
+// function solution(n){
+//     return (n+"").split("").reduce((acc, curr) => acc + parseInt(curr), 0)
+// }
