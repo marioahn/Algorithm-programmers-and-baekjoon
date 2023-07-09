56af51f79cff5747c5748de76a044d9bdd7fe18b
marioahn <89332401+marioahn@users.noreply.github.com>
2023-07-09 19:45:00 +0900

    [level 0] Title: A로 B 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,19 +1,4 @@
-// 순서를 바꾼다는게.. 무슨 말이지? 무슨 의미일까
-  // 아. 그냥 sort해서 다른게 나오면 false일듯?
-// let a = "olleh"
-// let b = a.split("")
-
-// console.log(b)
-// console.log(b.sort())
-// console.log(b.sort((a,b)=>a-b)) <- 이건 안되네. 어찌보면 당연하지 문자인데..숫자처럼 비교가안되잖아
-
 function solution(before, after) {
-  let beforeArr = before.split("").sort()
-  let afterArr = after.split("").sort()
-  
-  for (let i=0; i<before.length; i++) {
-    if (beforeArr[i] !== afterArr[i]) return 0
-  }
-  
-  return 1
+  let answer = (before.split("").sort().join("")===after.split("").sort().join(""))
+  return (answer) ? 1 : 0
 }
