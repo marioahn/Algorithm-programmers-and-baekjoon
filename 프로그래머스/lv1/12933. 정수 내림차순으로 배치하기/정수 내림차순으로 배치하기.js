fdd430e9ea3e67a897ae4b864c756378f5d86970
marioahn <89332401+marioahn@users.noreply.github.com>
2023-07-09 19:11:13 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -11,3 +11,26 @@ function solution(n) {
 
   return parseInt(answer);
 }
+
+//https://celltong.tistory.com/entry/JavaScript-sort-%EB%A9%94%EC%86%8C%EB%93%9C%EB%A1%9C-%EB%B0%B0%EC%97%B4-%EC%A0%95%EB%A0%AC%ED%95%98%EA%B8%B0
+//이거 읽고 정리 절.대 arr.sort()이런식으로쓰면 안되고, 제대로 정렬할 수 있는 함수를 같이 넣어줘야함
+
+// function solution(n) {
+//   const newN = n + "";
+//   const newArr = newN
+//     .split("")
+//     .sort()
+//     .reverse()
+//     .join("");
+
+//   return +newArr; // <- 와 이거 +는 그럼 또 숫자화시켜주는건가??
+// }
+// console.log(typeof +("123")) <-- 시바 number네;
+
+// re풀이!!
+// function solution(n) {
+//   let arr = String(n).split("").sort((a,b) => b-a)
+//   let reStr = arr.join("")
+
+//   return Number(reStr)
+// }
