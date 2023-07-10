f80fd8cc0a47ed62a72f52e9b656ab64e1419cf1
marioahn <89332401+marioahn@users.noreply.github.com>
2023-07-10 23:46:09 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 4.79 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,19 +1,18 @@
-// 이미 있는지 없는지 정보를 어케 알아내는가? 새 배열에 담아두기
-  // 또한, 가장 최근의 index정보까지 담는다!? 일석이조??
-  // key-value 객체로?
-
 function solution(s) {
-  let bucket = {}
-  let answer = []
+  let answer = [];
   
-  for (let i =0; i<s.length; i++) {
-    if (bucket[s[i]] === undefined) {
-      bucket[s[i]] = i 
-      answer.push(-1)
-    } else {
-      answer.push(i-bucket[s[i]])
-      bucket[s[i]] = i 
-    }
+  for (let i = 0; i < s.length; i++) {
+
+      let abc = -1
+
+      for (let j = i - 1; j >= 0; j--) {
+
+          if (s[j] === s[i]) {
+              abc = i - j
+              break
+          }
+      }
+      answer.push(abc)
   }
-  return answer
+  return answer;
 }
