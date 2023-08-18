b5d77c9a471ba0eeea470edbc66280343a33739b
marioahn <89332401+marioahn@users.noreply.github.com>
2023-08-18 10:12:02 +0900

    [level 1] Title: 약수의 합, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -1,10 +1,9 @@
 function solution(n) {
-    var answer = 0;
-    for (let i=0; i<=n; i++) {
-      if (n%i===0) {
-        answer += i
-      }
+  var answer = 0;
+  for (let i=0; i<=n; i++) {
+    if (n%i===0) {
+      answer += i
     }
-    
-    return answer;
+  }
+  return answer;
 }
