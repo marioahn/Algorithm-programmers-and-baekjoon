4832ef50909b445713fed92748714a14fc755997
marioahn <89332401+marioahn@users.noreply.github.com>
2023-08-19 01:21:58 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,14 +1,11 @@
-// '02' -> Number('02') 하면 어떤가?
-// '3141592', '271' -> t의 길이는 7, p의 길이는 3
-// (012) (123) (234) (345) (456) 
-
 function solution(t, p) {
-  let cnt = 0;
+    let arr = [];
+    let t_arr = [...t.split('')];
 
-  for (let i=0; i<=t.length-p.length; i++) {
-    let sliceNum = +(t.slice(i,i+p.length)) // +로 숫자화
-    if (sliceNum <= (+p)) cnt ++
-  }
-  
-  return cnt
+    for (let i = 0; i <= t.length - p.length; i++) {
+        arr.push(t_arr.slice(i, p.length + i).join(''))
+    };
+
+    return arr.filter(x => x <= (+p)).length;  
+
 }
