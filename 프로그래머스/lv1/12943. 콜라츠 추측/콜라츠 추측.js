2f0f12a7f1b5ac5e9e0bbd3e578c99a3a84fcbc9
marioahn <89332401+marioahn@users.noreply.github.com>
2023-08-18 20:28:39 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,13 +1,12 @@
 function solution(num) {
-
-  if (num === 1) { return 0 }
-
-  let n = 0
-  while (num !== 1) {
-    if (num % 2 === 0) { num = num/2 }
-    else { num = num*3+1 }
-    n ++ 
+  if (num === 1) return 0;
+  for (let i = 1; i <= 500; i++) {
+    if (num % 2 === 0) {
+      num = num / 2;
+    } else {
+      num = num * 3 + 1;
+    }
+    if (num === 1) return i;
   }
-
-  return n < 500 ? n : -1
+  return -1;
 }
