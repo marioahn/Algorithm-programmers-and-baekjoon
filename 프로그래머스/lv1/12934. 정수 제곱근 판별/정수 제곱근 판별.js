a85405f72fc7b5c9a3aa1083ef126d4e249316f5
marioahn <89332401+marioahn@users.noreply.github.com>
2023-08-18 11:15:58 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -2,10 +2,16 @@
   // 반복문 돌려서 될때까지 하는건 너무 비효율..뭐더라
   // 유클리드 호제법? ㄴㄴ
 
-function solution(n) {
-  let answer = 0
-  let sqrtN = Math.sqrt(n)
-  Number.isInteger(sqrtN) === true ? answer = Math.pow(sqrtN+1,2) : answer = -1
+// function solution(n) {
+//   let answer = 0
+//   let sqrtN = Math.sqrt(n)
+//   Number.isInteger(sqrtN) === true ? answer = Math.pow(sqrtN+1,2) : answer = -1
 
-  return answer
+//   return answer
+// }
+function solution(n) {
+
+  let sqrtN = Math.sqrt(n)
+
+  return Number(sqrtN) === parseInt(sqrtN) ? (sqrtN+1) ** 2 : -1
 }
