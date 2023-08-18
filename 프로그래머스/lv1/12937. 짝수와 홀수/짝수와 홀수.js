472ba6f9feee348097f3a74183aa4589619a4740
marioahn <89332401+marioahn@users.noreply.github.com>
2023-08-18 19:23:33 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 32.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -1,10 +1,3 @@
 function solution(num) {
-  // var answer = '';
-  // if (num % 2) {
-  //     answer = 'Odd'
-  // } else {
-  //     answer = 'Even'
-  // }
-  // return answer;
-  return num % 2 ? 'Odd' : 'Even'
+    return num % 2 ? "Odd" : "Even"
 }
