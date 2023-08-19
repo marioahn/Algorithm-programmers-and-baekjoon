1c7ab462181eeb15148e82edaec39cdb815d5793
marioahn <89332401+marioahn@users.noreply.github.com>
2023-08-19 16:07:46 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,20 +1,32 @@
+// function solution(s) {
+//   let arr = [
+//     "zero",
+//     "one",
+//     "two",
+//     "three",
+//     "four",
+//     "five",
+//     "six",
+//     "seven",
+//     "eight",
+//     "nine",
+//   ];
+//   for (let i = 0; i < arr.length; i++) {
+//     let spl = s.split(arr[i]);
+//     s = spl.join(i);
+//   }
+
+//   return parseInt(s);
+// }
 function solution(s) {
-  let arr = [
-    "zero",
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
-  ];
-  for (let i = 0; i < arr.length; i++) {
-    let spl = s.split(arr[i]);
-    s = spl.join(i);
+  let bucket = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+  // include, replace활용
+  for (let i=0; i<10; i++) {
+    if (s.includes(bucket[i])) { // true이면,
+      s = s.replaceAll(bucket[i],i) // s.replace만 하면x 새로운걸 반환하는 메서드
+    }
   }
 
-  return parseInt(s);
+  return Number(s)
 }
