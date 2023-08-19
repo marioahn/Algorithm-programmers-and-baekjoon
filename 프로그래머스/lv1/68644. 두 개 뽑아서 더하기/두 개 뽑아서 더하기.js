98e6fa494c5c21438ab6ff81aae0d1ba736b8d5f
marioahn <89332401+marioahn@users.noreply.github.com>
2023-08-19 16:37:53 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.33 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,15 +1,29 @@
-function solution(numbers) {
-  let answer = [];
+// function solution(numbers) {
+//   let answer = [];
 
-  for (let i = 0; i < numbers.length - 1; i++) {
-    for (let j = i + 1; j < numbers.length; j++) {
-      tmpSum = numbers[i] + numbers[j];
-      if (answer.indexOf(tmpSum) === -1) {
-        answer.push(tmpSum);
-      }
+//   for (let i = 0; i < numbers.length - 1; i++) {
+//     for (let j = i + 1; j < numbers.length; j++) {
+//       tmpSum = numbers[i] + numbers[j];
+//       if (answer.indexOf(tmpSum) === -1) {
+//         answer.push(tmpSum);
+//       }
+//     }
+//   }
+//   answer.sort((a, b) => a - b);
+
+//   return answer;
+// }
+function solution(numbers) {
+  let answer = new Set() // add로 +
+
+  // 반복문
+  for (let i=0; i<numbers.length-1; i++) {
+    for (let j=i+1; j<numbers.length; j++) {
+      let tmpSum = numbers[i] + numbers[j]
+      answer.add(tmpSum)
     }
   }
-  answer.sort((a, b) => a - b);
 
-  return answer;
+
+  return [...answer].sort((a,b)=>a-b)
 }
