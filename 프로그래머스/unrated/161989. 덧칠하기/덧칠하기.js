e6b7cf535026f4dbc089facf30dea843c7924685
marioahn <89332401+marioahn@users.noreply.github.com>
2023-08-22 11:24:42 +0900

    [unrated] Title: 덧칠하기, Time: 4547.83 ms, Memory: 40.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -0,0 +1,27 @@
+// 1. 길이가 n이고, 1로 채워진 배열을 만든다. section에 있는 번호들의 숫자는 0을 push.
+// 2. 0이 있는 index부터 m개만큼 1로 만든다. 근데, 룰러가 벽을 넘어가면 안됨.
+// 3. 배열에 0이 없어질때까지 반복한다.
+
+// 수정
+// 1. 길이가 n이고, 1로 채워진 배열을 만든다. section에 있는 번호들의 숫자는 0을 push.
+// 2. 위에서 만든 배열을 돌면서 0을 만나면 / 칠하는 횟수 +1 / index를 m만큼 건너뜀
+// 3. arrN이 0이 아니면 index를 1만 추가
+
+function solution (n, m, section){
+  let arrN = []
+  for (let i=1; i<=n; i++) {
+    if (section.includes(i) > 0) arrN.push(0)
+    else arrN.push(1)
+  }
+  // [1, 0, 0, 1, 1, 0, 1, 1] 
+  // [0, 1, 0, 1, 1]
+  // [0, 0, 0, 0]
+  let cnt = 0
+  for (let i = 0; i < n; i++) {
+    if (arrN[i] === 0) {
+      cnt ++
+      i += m - 1
+    }
+  }
+  return cnt
+}
