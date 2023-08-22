56a2a72c1998bb2acce6c738ab8f5aa58b275cc4
marioahn <89332401+marioahn@users.noreply.github.com>
2023-08-22 12:12:11 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,39 +1,62 @@
-function order(number) {
-  if (number === 6) {
-    return 1;
-  }
-  if (number === 5) {
-    return 2;
-  }
-  if (number === 4) {
-    return 3;
-  }
-  if (number === 3) {
-    return 4;
-  }
-  if (number === 2) {
-    return 5;
-  }
-  if (number === 1) {
-    return 6;
-  }
-  if (number === 0) {
-    return 6;
-  }
-}
+// function order(number) {
+//   if (number === 6) {
+//     return 1;
+//   }
+//   if (number === 5) {
+//     return 2;
+//   }
+//   if (number === 4) {
+//     return 3;
+//   }
+//   if (number === 3) {
+//     return 4;
+//   }
+//   if (number === 2) {
+//     return 5;
+//   }
+//   if (number === 1) {
+//     return 6;
+//   }
+//   if (number === 0) {
+//     return 6;
+//   }
+// }
 
+// function solution(lottos, win_nums) {
+//   // step1: 최저부터 - 0인 경우 모두 continue(로또번호는 1부터이므로 자동으로 걸러짐)
+//   // step2: 최고순위 -> minCnt + zeroCnt
+//   let minCnt = 0;
+//   let zeroCnt = 0;
+//   for (let i = 0; i < lottos.length; i++) {
+//     if (win_nums.indexOf(lottos[i]) >= 0) {
+//       minCnt++;
+//     }
+//     if (lottos[i] === 0) {
+//       zeroCnt++;
+//     }
+//   }
+//   return [order(minCnt + zeroCnt), order(minCnt)];
+// }
 function solution(lottos, win_nums) {
-  // step1: 최저부터 - 0인 경우 모두 continue(로또번호는 1부터이므로 자동으로 걸러짐)
-  // step2: 최고순위 -> minCnt + zeroCnt
-  let minCnt = 0;
-  let zeroCnt = 0;
-  for (let i = 0; i < lottos.length; i++) {
+
+  let minAnswer = 0
+  let zeroCnt = 0
+  let minRank = 0
+  let maxRank = 0
+  for (let i=0; i<6; i++) {
     if (win_nums.indexOf(lottos[i]) >= 0) {
-      minCnt++;
+      minAnswer ++
     }
-    if (lottos[i] === 0) {
-      zeroCnt++;
-    }
+    if (lottos[i] === 0) zeroCnt ++
   }
-  return [order(minCnt + zeroCnt), order(minCnt)];
+
+  // minRank
+  minAnswer === 1 || minAnswer === 0? minRank = 6 : minRank = 7 - minAnswer
+
+  // maxRank
+  minAnswer + zeroCnt === 1 || minAnswer + zeroCnt === 0? maxRank = 6 : maxRank = 7 - (minAnswer + zeroCnt)
+
+  // console.log(7 - (minAnswer + zeroCnt), minRank) // 최고, 최저 순
+
+  return [maxRank, minRank]
 }
