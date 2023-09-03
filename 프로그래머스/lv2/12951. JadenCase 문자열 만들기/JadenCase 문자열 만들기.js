ce3985e3da33650cce707506b6f6a419eac9b146
marioahn <89332401+marioahn@users.noreply.github.com>
2023-09-03 14:05:54 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,26 +1,3 @@
-// 3트
 function solution(s) {
-  let answer = ''
-  let stack = ''
-
-  for (let i =0; i<s.length; i++) {
-    if (s[i] !== ' ') {
-      if (stack === '') { // 공백 다음의 첫 문자인 경우
-        stack += s[i].toUpperCase() 
-      } else {
-        stack += s[i].toLowerCase()
-      }
-    } else { // s[i]가 ' ' -> 공백이면
-      answer += stack + ' '
-      stack = ''
-    }
-  }
-
-  answer += stack // 마지막 단어 추가
-
-  return answer
+    return s.split(" ").map(v => v.charAt(0).toUpperCase() + v.substring(1).toLowerCase()).join(" ");
 }
-
-
-console.log(solution("3people unFollowed me"))
-console.log(solution("for the last week"))
