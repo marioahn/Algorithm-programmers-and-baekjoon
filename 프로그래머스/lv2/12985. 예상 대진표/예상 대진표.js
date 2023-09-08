b21c76bec5316383626cff591dd1dd065ab3cfd8
marioahn <89332401+marioahn@users.noreply.github.com>
2023-09-08 15:40:55 +0900

    [level 2] Title: 예상 대진표, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -1,13 +1,47 @@
 
-function solution(n,a,b) {
-  
-  let cnt = 0
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+function solution(n,a,b)
+{
+    if (a > b) {
+        var t = b;
+        b = a;
+        a = t;
+    }
+    var times = 1;
 
-  while (a !== b) {
-    a = Math.ceil(a/2)
-    b = Math.ceil(b/2)
-    cnt ++ 
-  }
+    while(a%2!==1 || b-a > 1) {
+        if (a % 2 === 1) a++;
+        if (b % 2 === 1) b++;
+        a = a/2;
+        b = b/2;
+        times++;
+    }
 
-  return cnt
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    //console.log('Hello Javascript')
+
+    return times;
 }
