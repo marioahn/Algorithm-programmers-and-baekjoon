1a31fab555b519d23553713263b920012c8a208e
marioahn <89332401+marioahn@users.noreply.github.com>
2023-09-11 03:58:14 +0900

    [unrated] Title: 귤 고르기, Time: 62.32 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
@@ -0,0 +1,21 @@
+// 2트: 
+function solution(k, tangerine) {
+  let obj = {};
+
+  for (let i=0; i<tangerine.length; i++) {
+    if (!obj[tangerine[i]]) obj[tangerine[i]] = 1
+    else obj[tangerine[i]] += 1
+  };
+
+  let values = Object.values(obj);
+  values.sort((a,b) => b-a)
+  
+  for (let i=0; i<values.length; i++) {
+    k -= values[i]
+    if (k <= 0) return i+1
+  }
+}
+
+console.log(solution(6,[1, 3, 2, 5, 4, 5, 2, 3,7]));
+console.log(solution(4,[1, 3, 2, 5, 4, 5, 2, 3]));
+console.log(solution(2,[1, 1, 1, 1, 2, 2, 2, 3]));
