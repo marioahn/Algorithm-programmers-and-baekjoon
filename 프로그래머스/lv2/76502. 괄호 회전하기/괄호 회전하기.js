d138dee683e21d980954a6912bb39ac64ee25688
marioahn <89332401+marioahn@users.noreply.github.com>
2023-09-12 22:47:06 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.03 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,26 +1,36 @@
-// 3트 - 성공
-  function solution(s) {
-    if (s.length % 2 === 1) return 0
-  
-    let cnt = 0
-    for (let i=0; i<s.length-1; i++) {
-      let tmp = s
-      // tmp값이 하나도 안 걸러질때까지, while문으로 반복
-      while (1) {
-        let tmp2 = tmp
-        tmp = tmp.replaceAll('()','')
-        tmp = tmp.replaceAll('{}','')
-        tmp = tmp.replaceAll('[]','')
-        if (tmp2 === tmp) break // replace로 걸러지는게 없다면? 그만해
-      }
-  
-      // 비로소, 모든 (),{},[]가 없어짐 - ('({})'같은포함형도 마찬가지)
-      if (tmp.length === 0) cnt ++
-      s = s.slice(1) + s[0]
-    }
+// 4트: 아 근데, 3트는 너무 시간 오래걸리고 비효율적이야
+  // stack으로 다시 풀어보자
+function solution(s) {
+  if (s.length % 2 === 1) return 0;
+
+  let pair = { '}': '{', ')': '(', ']':'['};
+  let cnt = 0;
+
+  // step1: s.length-1번 왼쪽으로 회전
+  for (let i=0; i<s.length-1; i++) {
+    let stack = [];
+    // step2: stack이용해서, 옳은 괄호형인지 체크
+    for (let j=0; j<s.length; j++) {
+      if (s[j] === '(' || s[j] === '[' || s[j] === '{') {
+        stack.push(s[j])
+      } 
+      else {
+        let recent = stack.pop()
+        // 맞지 않으면, 해당 로테는 실패 -> cnt-1하고 break
+        if (recent !== pair[s[j]]) {
+          cnt -= 1
+          break
+        }
+      };
+      // break안 걸리고, 끝까지 왔다면? 성공
+    };
     
-    return cnt
-  }
+    cnt ++ 
+    s = s.slice(1) + s[0];
+  };
+
+  return cnt;
+};
 
 console.log(solution("[](){}"));
 console.log(solution("}]()[{"));
