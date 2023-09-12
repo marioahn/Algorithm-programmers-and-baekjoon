2751438e8cf9264c71ea77eca05064a06669829e
marioahn <89332401+marioahn@users.noreply.github.com>
2023-09-12 21:50:08 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.02 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,10 +1,11 @@
-// 3트
+// 3트 - 성공
   function solution(s) {
     if (s.length % 2 === 1) return 0
   
     let cnt = 0
     for (let i=0; i<s.length-1; i++) {
       let tmp = s
+      // tmp값이 하나도 안 걸러질때까지, while문으로 반복
       while (1) {
         let tmp2 = tmp
         tmp = tmp.replaceAll('()','')
@@ -13,6 +14,7 @@
         if (tmp2 === tmp) break // replace로 걸러지는게 없다면? 그만해
       }
   
+      // 비로소, 모든 (),{},[]가 없어짐 - ('({})'같은포함형도 마찬가지)
       if (tmp.length === 0) cnt ++
       s = s.slice(1) + s[0]
     }
