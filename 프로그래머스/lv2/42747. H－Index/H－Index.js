d38faa0152964bcc5e8c20ede92ad71c1d8aa3df
marioahn <89332401+marioahn@users.noreply.github.com>
2023-09-14 22:04:08 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -1,23 +1,7 @@
 function solution(citations) {
-  let Max = Math.max(...citations);
+    let i = 0;
 
-  let answer = 0
-  for (let i=1; i<=Max; i++) {
-    let [up, down] = [0,0];
+    while(i + 1 <= citations.sort((a, b) => b - a)[i]) i++;
 
-    for (let j=0; j<citations.length; j++) {
-      if (citations[j] >= i) {
-        up ++
-      }
-      if (citations[j] <= i) {
-        down ++
-      }
-    };
-
-    if (up>=i && down<=i) answer = i
-  }
-
-  return answer
+    return i;
 }
-
-console.log(solution([3, 0, 6, 1, 5]))
