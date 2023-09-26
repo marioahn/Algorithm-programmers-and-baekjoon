e80fd457f8d2490da3601f440fab02be71dc9b36
marioahn <89332401+marioahn@users.noreply.github.com>
2023-09-26 19:32:56 +0900

    [level 2] Title: 모음 사전, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
@@ -1,27 +1,16 @@
-// 1트: DFS로 풀이 -> BUT, 다른 풀이에 비해 오래 걸림
-
+// 2트: 레퍼런스 코드 참조해서 다시 풀이
 function solution(word) {
-  let answer = 0
+  // 초기값 - ex) AAA는 3이며, AAE는 5**2+5**1+5**0 + '3'이다
+  // 그니까, AAE는 3자리수의 원형인 3에서부터 규칙에 따라 추가되므로, cnt의 초깃값을 word.length로 잡는다
+  let cnt = word.length 
   let alphabets = ['A','E','I','O','U']
+  let interval = [781, 156, 31, 6, 1]
 
-  function dfs(sum) {
-    answer += 1
+  for (let i=0; i<word.length; i++) {
+    cnt += alphabets.indexOf(word[i]) * interval[i]
+  }
 
-    if (sum === word) { return true };
-    if (sum.length === 5) { return false };
-
-    for (let alphabet of alphabets) {
-      if (dfs(sum+alphabet) === true) {
-        return true
-      }
-    }
-  };
-
-  for (let alphabet of alphabets) {
-    if (dfs(alphabet) === true) {
-      return answer
-    }
-  };
-};
+  return cnt
+}
 
 console.log(solution('AAAAE'))
