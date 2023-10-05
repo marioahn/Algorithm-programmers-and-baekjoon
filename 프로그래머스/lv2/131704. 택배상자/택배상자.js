0a823eba335cbd07f9cafd5c8574d0f8770a8311
marioahn <89332401+marioahn@users.noreply.github.com>
2023-10-05 15:33:22 +0900

    [level 2] Title: 택배상자, Time: 91.98 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv2/131704. 택배상자/택배상자.js
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.js
@@ -3,7 +3,9 @@ function solution(order) {
   let subContainer = []
   let [cnt, orderN] = [0,0] // order배열의 작은 수는 1임. 0이 아니라
 
+  // (main)컨테이너 상자 꺼내기
   for (let i=1; i<=order.length; i++) {
+    // step1: 메인 확인
     if (order[orderN] !== i) {
       subContainer.push(i)
     } else {
@@ -11,7 +13,7 @@ function solution(order) {
       cnt ++
     }
 
-    // stack(보조)에 상자가 남아있고, 맨 뒤에 넣은 상자와 번호가 같으면 cnt
+    // step2: stack(보조)에 상자가 남아있고, 맨 뒤에 넣은 상자와 번호가 같으면 cnt
     while (subContainer.length && subContainer.at(-1) === order[orderN]) {
       subContainer.pop();
       orderN ++
