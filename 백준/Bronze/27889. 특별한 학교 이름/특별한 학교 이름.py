ba4808d45464d6f745ef7444511bafb5db6ddb81
marioahn <89332401+marioahn@users.noreply.github.com>
2023-10-17 16:52:43 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,14 @@
+# abbreviation = input()
+# if abbreviation == 'NLCS':
+#     print('North London Collegiate School')
+# else if abbreviation == 'NLCS':
+#     print('North London Collegiate School')
+# ...
+
+# 컷;;
+schools = ['North London Collegiate School', 'Branksome Hall Asia',
+           'Korea International School', 'St. Johnsbury Academy']
+abbreviation = ['NLCS', 'BHA', 'KIS', 'SJA']
+
+school = input()
+print(schools[abbreviation.index(school)])
