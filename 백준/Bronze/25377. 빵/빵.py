79de63b78cb91ce90322501aab3bb7d0a6db369f
marioahn <89332401+marioahn@users.noreply.github.com>
2023-10-26 22:59:45 +0900

    [Bronze IV] Title: 빵, Time: 44 ms, Memory: 31120 KB, Score: 16 point -BaekjoonHub

---
+++ 백준/Bronze/25377. 빵/빵.py
@@ -0,0 +1,11 @@
+N = int(input())
+minNumber = 1001
+for _ in range(N):
+    a, b = map(int, input().split())
+    if a < b:
+        minNumber = min(minNumber, b)
+
+if minNumber == 1001:
+    print(-1)
+else:
+    print(minNumber)
