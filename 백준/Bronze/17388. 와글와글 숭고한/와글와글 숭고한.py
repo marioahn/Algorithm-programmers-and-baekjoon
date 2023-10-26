3ff04118a26732e27c508257f20bff1fb26400b3
marioahn <89332401+marioahn@users.noreply.github.com>
2023-10-26 21:43:01 +0900

    [Bronze IV] Title: 와글와글 숭고한, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.py
@@ -0,0 +1,13 @@
+a, b, c = map(int, input().split())
+
+bucket = ['Soongsil', 'Korea', 'Hanyang']
+
+if (a+b+c) >= 100:
+    print('OK')
+else:
+    if min(a, b, c) == a:
+        print('Soongsil')
+    elif min(a, b, c) == b:
+        print('Korea')
+    else:
+        print('Hanyang')
