0d7fe8e50ef968e1f6124552588506a316b10aba
marioahn <89332401+marioahn@users.noreply.github.com>
2024-01-13 18:44:16 +0900

    [Silver V] Title: FIFA 월드컵, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4884. FIFA 월드컵/FIFA 월드컵.py
@@ -0,0 +1,17 @@
+import sys
+while True:
+    g, t, a, d = map(int, sys.stdin.readline().split())
+    if (g == -1):
+        break
+    pre = t*(t-1)//2*g
+    knockout = g*a+d
+    x, y = 0, 0
+    temp = 1
+    while knockout > temp:
+        x += temp
+        temp *= 2
+    
+    y += temp-knockout
+    x += pre
+
+    print(f"{g}*{a}/{t}+{d}={x}+{y}")
