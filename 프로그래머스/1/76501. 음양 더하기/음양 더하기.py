7d8dc5ead39ab63f6943046fa7c280fdfbd4fdf8
marioahn <89332401+marioahn@users.noreply.github.com>
2024-02-23 11:53:46 +0900

    [level 1] Title: 음양 더하기, Time: 0.11 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -7,3 +7,9 @@ def solution(absolutes, signs):
             result += absolutes[i]
             
     return result  
+
+# 한 줄 코딩 생각해봤는데 잘 안되었음 -> 아래 참고 굿
+# for문, if문과 2개 변수를 동시에 사용하는 방법
+# return sum(absolutes if sign else -absolutes for absolutes, sign in zip(absolutes, signs))
+
+
