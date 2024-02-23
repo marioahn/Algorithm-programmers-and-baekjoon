db5936ad5f42005a9716f00ccd3342bc2758a305
marioahn <89332401+marioahn@users.noreply.github.com>
2024-02-23 12:39:03 +0900

    [level 1] Title: 내적, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.py
+++ 프로그래머스/1/70128. 내적/내적.py
@@ -1,7 +1,10 @@
-def solution(a, b):
-    # return sum([i*j for i in a for j in b]) # 이중포문이기 때문에 x
-    # return sum([lambda key=i,j: for i in a for j in b]) # 문법오류
-    sum = 0
-    for k in range(len(a)):
-        sum += a[k]*b[k]
-    return sum
+# def solution(a, b):
+#     # return sum([i*j for i in a for j in b]) # 이중포문이기 때문에 x
+#     # return sum([lambda key=i,j: for i in a for j in b]) # 문법오류
+#     sum = 0
+#     for k in range(len(a)):
+#         sum += a[k]*b[k]
+#     return sum
+
+def solution(a,b):
+    return sum([x*y for x,y in zip(a,b)])
