cd2f9f7b4277004d971618c61096a619b09fe0ab
marioahn <89332401+marioahn@users.noreply.github.com>
2024-02-23 11:19:01 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
@@ -1,6 +1,10 @@
-def solution(x):
-    sum = 0
-    for ele in str(x):
-        sum += int(ele)
+# def solution(x):
+#     sum = 0
+#     for ele in str(x):
+#         sum += int(ele)
     
-    return False if (x % sum) else True
+#     return False if (x % sum) else True
+
+
+def solution(n):
+    return n%sum(int(x) for x in str(n)) == 0
