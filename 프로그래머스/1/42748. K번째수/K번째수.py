4ecb4f17dd358d7e6e06642414d8fdacad19db23
marioahn <89332401+marioahn@users.noreply.github.com>
2024-02-28 23:08:16 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,14 +1,18 @@
-def solution(array, commands):
-    answer = []
+# def solution(array, commands):
+#     answer = []
 
-    for k in range(len(commands)):
-        slice_arr = array[commands[k][0]-1:commands[k][1]]
-        slice_arr.sort()
+#     for k in range(len(commands)):
+#         slice_arr = array[commands[k][0]-1:commands[k][1]]
+#         slice_arr.sort()
         
-        answer.append(slice_arr[commands[k][2]-1])
+#         answer.append(slice_arr[commands[k][2]-1])
 
-    return answer
+#     return answer
 
 
-print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
+# print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
 
+# 레퍼런스
+def solution(array, commands):
+    # return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands)
+    return [sorted(array[a-1:b])[c-1] for a,b,c in commands]
