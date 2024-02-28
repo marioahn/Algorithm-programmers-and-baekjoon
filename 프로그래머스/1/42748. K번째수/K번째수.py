1ceb66ead806c4f65b490a7703b46257c1da7823
marioahn <89332401+marioahn@users.noreply.github.com>
2024-02-28 23:04:36 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,14 @@
+def solution(array, commands):
+    answer = []
+
+    for k in range(len(commands)):
+        slice_arr = array[commands[k][0]-1:commands[k][1]]
+        slice_arr.sort()
+        
+        answer.append(slice_arr[commands[k][2]-1])
+
+    return answer
+
+
+print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
+
