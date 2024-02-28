0f1de0632c2e3961bf57fe20f8d1bd213e0955fc
marioahn <89332401+marioahn@users.noreply.github.com>
2024-02-28 22:41:17 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -8,6 +8,13 @@ def solution(s):
     
     return int(s)
 
-
-
 print(solution("one4seveneight"))
+
+# 레퍼런스 - wow;;;;;;;
+# num_dic = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+
+# def solution(s):
+#     answer = s
+#     for key, value in num_dic.items():
+#         answer = answer.replace(key, value)
+#     return int(answer)
