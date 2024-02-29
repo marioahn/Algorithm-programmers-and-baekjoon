3c25935ccc1c97767eb0ea874b75eb74cdf7df62
marioahn <89332401+marioahn@users.noreply.github.com>
2024-02-29 16:17:22 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 134.27 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,5 +1,6 @@
 # 파이썬으로만 할 수 있는 깔끔한 방법이 있을것 같은데 흠
 # O(N**2)로 밖에 안되나 - 
+# dict사용하면 가능 - 및, stack.append(ele)는 그냥 if-else문 밖에 한줄만 써도 ok
 def solution(s):
     stack = [] # idx찾을 용도(임시)
     result = []
@@ -14,6 +15,17 @@ def solution(s):
 
     return result
 
+# def solution(s):
+#     answer = []
+#     dic = dict()
+#     for i in range(len(s)):
+#         if s[i] not in dic:
+#             answer.append(-1)
+#         else:
+#             answer.append(i - dic[s[i]])
+#         dic[s[i]] = i
+
+#     return answer
 
 print(solution("banana")) # [-1, -1, -1, 2, 2, 2]
 print(solution("foobar")) # [-1, -1, 1, -1, -1, -1]
