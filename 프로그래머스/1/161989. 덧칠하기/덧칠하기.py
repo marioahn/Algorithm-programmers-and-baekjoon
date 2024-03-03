9bd302d5d6a084a2fb4c18310998c384f84910c2
marioahn <89332401+marioahn@users.noreply.github.com>
2024-03-04 00:36:36 +0900

    [level 1] Title: 덧칠하기, Time: 11.57 ms, Memory: 12.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -0,0 +1,18 @@
+def solution(n, m, section):
+    cnt = 0
+
+    cleaning = [0 for _ in range(m)]
+    bucket = [0 for _ in range(n)]
+    # bucket에서 훼손구역 1로 표시하기
+    for ele in section:
+        bucket[ele-1] = 1
+
+		# cleaning
+    for i in range(n):
+        if bucket[i] == 1:
+            bucket[i:i+m] = cleaning
+            cnt += 1
+    
+    return cnt
+
+print(solution(8,4,[2, 3, 6]))
