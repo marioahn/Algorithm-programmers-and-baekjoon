d55bc79f1b09776447b769ffa5dacfb874e280d9
marioahn <89332401+marioahn@users.noreply.github.com>
2024-03-04 01:00:42 +0900

    [level 1] Title: 기사단원의 무기, Time: 2.90 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,27 @@
+# 1. 약수개수
+    # https://velog.io/@dmsgur7112/Python-%EC%95%BD%EC%88%98-%EA%B5%AC%ED%95%98%EA%B8%B0
+# 2. limit넘기는 것은 -> limit-2로 변환(아니면 2고정?)
+
+def divisor(n): # 16 -> [1,2,4,8,16]
+    cnt = 0
+    for i in range(1,int(n**(1/2))+1):
+        if n % i == 0:
+            cnt += 1
+            if i < (n//i): # 1,16 / 2,8은 쌍인데, 4는 한개이므로 이 조건 필요
+                cnt += 1
+
+    return cnt
+
+def solution(number, limit, power):
+    sum = 0
+    for k in range(1,number+1):
+        if divisor(k) <= limit:
+            sum += divisor(k)
+        else:
+            sum += power
+
+    return sum
+
+
+print(solution(5,3,2))
+print(solution(10,3,2))
