91f6c32b2f65be8380382e1e4cfe02deded4be36
marioahn <89332401+marioahn@users.noreply.github.com>
2024-03-04 00:40:23 +0900

    [level 1] Title: 덧칠하기, Time: 1.72 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,18 +1,29 @@
-def solution(n, m, section):
-    cnt = 0
+# def solution(n, m, section):
+#     cnt = 0
 
-    cleaning = [0 for _ in range(m)]
-    bucket = [0 for _ in range(n)]
-    # bucket에서 훼손구역 1로 표시하기
-    for ele in section:
-        bucket[ele-1] = 1
+#     cleaning = [0 for _ in range(m)]
+#     bucket = [0 for _ in range(n)]
+#     # bucket에서 훼손구역 1로 표시하기
+#     for ele in section:
+#         bucket[ele-1] = 1
 
-		# cleaning
-    for i in range(n):
-        if bucket[i] == 1:
-            bucket[i:i+m] = cleaning
-            cnt += 1
+# 		# cleaning
+#     for i in range(n):
+#         if bucket[i] == 1:
+#             bucket[i:i+m] = cleaning
+#             cnt += 1
     
-    return cnt
+#     return cnt
 
-print(solution(8,4,[2, 3, 6]))
+# print(solution(8,4,[2, 3, 6]))
+
+# 레퍼런스
+def solution(n, m, section):
+    answer = 1
+    prev = section[0]
+    for sec in section:
+        if sec - prev >= m:
+            prev = sec
+            answer += 1
+
+    return answer
