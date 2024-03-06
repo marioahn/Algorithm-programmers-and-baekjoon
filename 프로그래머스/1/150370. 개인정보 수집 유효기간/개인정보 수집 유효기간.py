5d98fa21cfa95fe1a13461e1041157de26612abb
marioahn <89332401+marioahn@users.noreply.github.com>
2024-03-06 18:58:32 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.22 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -31,3 +31,18 @@ def solution(today, terms, privacies):
 
 print(solution("2022.05.19",["A 6", "B 12", "C 3"],["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"])) # [1, 3]
 print(solution("2020.01.01",["Z 3", "D 5"],["2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"])) # [1, 4, 5]
+
+
+# 레퍼런스 - 축약인데도, 가독성이 좋음. 진짜 두고두고 봐서 익힐 코드인듯 이건
+# def to_days(date):
+#     year, month, day = map(int, date.split("."))
+#     return year * 28 * 12 + month * 28 + day
+
+# def solution(today, terms, privacies):
+#     months = {v[0]: int(v[2:]) * 28 for v in terms}
+#     today = to_days(today)
+#     expire = [
+#         i + 1 for i, privacy in enumerate(privacies)
+#         if to_days(privacy[:-2]) + months[privacy[-1]] <= today
+#     ]
+#     return expire
