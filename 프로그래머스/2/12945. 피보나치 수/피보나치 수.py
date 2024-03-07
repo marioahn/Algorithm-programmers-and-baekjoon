9f786fe65f1254f611967beba2684cdbd532693e
marioahn <89332401+marioahn@users.noreply.github.com>
2024-03-07 17:44:35 +0900

    [level 2] Title: 피보나치 수, Time: 503.50 ms, Memory: 439 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,10 +1,16 @@
 def solution(n):
     pibo = [0,1]
-
     for k in range(1,n):
         pibo.append(pibo[k-1]+pibo[k])
 
     return pibo[-1] % 1234567
 
-
 print(solution(5))
+
+# 레퍼런스
+    # 피보나치 아래처럼 swap이용해서 할 수도 있네 ㅎㄷㄷ / 공간차지 안해서 좋은듯
+# def fibonacci(num):
+#     a, b = 0, 1
+#     for i in range(num):
+#         a, b = b, a+b
+#     return a
