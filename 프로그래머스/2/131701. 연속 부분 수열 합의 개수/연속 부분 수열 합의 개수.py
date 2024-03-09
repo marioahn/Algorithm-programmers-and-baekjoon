0a83819ca26892f5792afc16cb7e49712f975f5a
marioahn <89332401+marioahn@users.noreply.github.com>
2024-03-09 18:02:21 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 248.91 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -18,19 +18,40 @@
 #     return len(set(result))
 
 # 2트 - 1트 개선
-    # 처음부터 set으로 / len(elements) -> 변수에 담아서 넣어주기
-def solution(elements):
-    # result = elements # 앗;; 이러면 얕은 복사라서 원본/복사본 둘다 바뀜
-    result = set(elements[:])
-    length = len(elements)
+    # (1)처음부터 set으로 / len(elements) -> 변수에 담아서 넣어주기
+        # -> 메모리는 엄청 줄음 - TC20: 통과 (4589.91ms, 43.7MB)
+    # (2)그럼에도 시간복잡도가 O(N**3)이기 때문에, 더 줄일 수가 없음
+        # 2중포문안에 sum함수.
+        # -> 따라서 근본적으로 풀이 구조를 바꿔야 함. 이 방식대론 답x
+
+# def solution(elements):
+#     # result = elements # 앗;; 이러면 얕은 복사라서 원본/복사본 둘다 바뀜
+#     result = set(elements[:])
+#     length = len(elements)
 
     
-    # k = 길아가 k인 연속수열의 경우, 2부터 시작
-    for k in range(2,length+1):
-        tmp = elements + elements[0:k-1]
-        for i in range(length):
-            result.add(sum(tmp[i:i+k]))
+#     # k = 길아가 k인 연속수열의 경우, 2부터 시작
+#     for k in range(2,length+1):
+#         tmp = elements + elements[0:k-1]
+#         for i in range(length):
+#             result.add(sum(tmp[i:i+k]))
 
-    return len(result)
+#     return len(result)
 
-print(solution([7,9,1,1,4]))
+# print(solution([7,9,1,1,4]))
+
+
+# 3트 - 레퍼런스
+    # 시간복잡도: O(N**2)
+    # 댓글: 길이 별로 잘라서 모으는게 아니라, 하나 주인공 잡고 거기서부터 모든 길이를 다 자르는 것
+def solution(elements):
+    ll = len(elements)
+    res = set()
+
+    for i in range(ll):
+        ssum = elements[i]
+        res.add(ssum)
+        for j in range(i+1, i+ll):
+            ssum += elements[j%ll]
+            res.add(ssum)
+    return len(res)
