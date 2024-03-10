d8eb202788d308693f1aaebef023eb35aff91c30
marioahn <89332401+marioahn@users.noreply.github.com>
2024-03-11 00:28:53 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -4,7 +4,6 @@
     # x1x2,x1c, ax2,ac, bx2,bc -> 여기서 x1x2는 빼야 함
     # 따라서 return (k1+1)*(k2+1)-1! 
 # clothes를 한방에 dict로 바꾸는 메서드 없을까
-# from functools import reduce
 from math import prod
 
 
@@ -21,5 +20,33 @@ def solution(clothes):
     value_list = [x+1 for x in value_list]
     return prod(value_list)-1
 
+
+
 print(solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]))
 print(solution([["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]))
+
+# 레퍼런스1 - 아. 너무 복잡하게 생각했다;;
+    # 1)중간에 +1해서 빈종류 생각하기전에, 걍 애초에 시작부터 수를 2로 두면 됨
+    # 2)곱하기 걍 반복문써도 되는데 끙;;
+# def solution(clothes):
+#     clothes_type = {}
+
+#     for c, t in clothes:
+#         if t not in clothes_type:
+#             clothes_type[t] = 2
+#         else:
+#             clothes_type[t] += 1
+
+#     cnt = 1
+#     for num in clothes_type.values():
+#         cnt *= num
+#     return cnt - 1
+
+# 레퍼런스2
+# def solution(clothes):
+#     from collections import Counter
+#     from functools import reduce
+#     cnt = Counter([kind for name, kind in clothes])
+#     answer = reduce(lambda x, y: x*(y+1), cnt.values(), 1) - 1
+    
+#     return cnt
