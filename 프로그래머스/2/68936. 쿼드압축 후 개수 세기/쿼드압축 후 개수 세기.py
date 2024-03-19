e42f5f993e3fd7050cda1a6da434375bf9760b8c
marioahn <89332401+marioahn@users.noreply.github.com>
2024-03-19 19:15:33 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 75.92 ms, Memory: 12.1 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
@@ -3,25 +3,48 @@
 # 직접 코드를 짜야 하나? 너무 비효율적인거 같은데.. 일단 풀고 레퍼런스 ㄱㄱ
 # 재귀로 계속 들어가야 할듯: 1024=2**10. arr의 길이 
 # import math # print(int(math.log2(1024)))
-def solution(arr):
-    result=[0,0]
-    length=len(arr)
+# def solution(arr):
+#     result=[0,0]
+#     length=len(arr)
     
-    def dfs(a,b,l):
-        start=arr[a][b]
-        for i in range(a,a+l):
-            for j in range(b,b+l):
-                if arr[i][j]!=start:
-                    l=l//2
-                    dfs(a,b,l)
-                    dfs(a,b+l,l)
-                    dfs(a+l,b,l)
-                    dfs(a+l,b+l,l)
-                    return
+#     def dfs(a,b,l):
+#         start=arr[a][b]
+#         for i in range(a,a+l):
+#             for j in range(b,b+l):
+#                 if arr[i][j]!=start:
+#                     l=l//2
+#                     dfs(a,b,l)
+#                     dfs(a,b+l,l)
+#                     dfs(a+l,b,l)
+#                     dfs(a+l,b+l,l)
+#                     return
                 
-        result[start]+=1
+#         result[start]+=1
         
-    dfs(0,0,length)
+#     dfs(0,0,length)
     
-    return (result)
+#     return (result)
 
+def solution(arr):
+    answer = [0, 0]
+
+    def check(size, x, y):
+        if size == 1:
+            answer[arr[y][x]] += 1
+            return
+        else:
+            first = arr[y][x]
+
+            for dy in range(size):
+                for dx in range(size):
+                    if first != arr[y + dy][x + dx]:
+                        check(size // 2, x, y)
+                        check(size // 2, x + size // 2, y)
+                        check(size // 2, x, y + size // 2)
+                        check(size // 2, x + size // 2, y + size // 2)
+                        return
+            answer[first] += 1
+    check(len(arr),0,0)
+
+
+    return answer
