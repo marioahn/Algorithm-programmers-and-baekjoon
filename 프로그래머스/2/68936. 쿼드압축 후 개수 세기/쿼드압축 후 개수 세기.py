850aec5617d30a8a592c80c888712a3e2447c153
marioahn <89332401+marioahn@users.noreply.github.com>
2024-03-19 19:55:29 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 65.79 ms, Memory: 12.2 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
@@ -25,26 +25,27 @@
     
 #     return (result)
 
-def solution(arr):
-    answer = [0, 0]
 
-    def check(size, x, y):
+def solution(arr):
+    answer = [0,0]
+
+    def check(size, x,y):
         if size == 1:
             answer[arr[y][x]] += 1
             return
+
         else:
             first = arr[y][x]
-
             for dy in range(size):
                 for dx in range(size):
-                    if first != arr[y + dy][x + dx]:
-                        check(size // 2, x, y)
-                        check(size // 2, x + size // 2, y)
-                        check(size // 2, x, y + size // 2)
-                        check(size // 2, x + size // 2, y + size // 2)
+                    if first != arr[y+dy][x+dx]:
+                        check(size//2, x,y)
+                        check(size//2, x,y+size//2)
+                        check(size//2, x+size//2,y)
+                        check(size//2, x+size//2,y+size//2)
                         return
             answer[first] += 1
-    check(len(arr),0,0)
-
+    
+    check(len(arr), 0,0)
 
     return answer
