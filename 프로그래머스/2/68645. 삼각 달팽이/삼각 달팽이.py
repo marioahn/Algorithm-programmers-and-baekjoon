f8de4010fc0f8de7ef11ca3015d7cd91f83d0e8e
marioahn <89332401+marioahn@users.noreply.github.com>
2024-03-21 15:11:01 +0900

    [level 2] Title: 삼각 달팽이, Time: 2670.33 ms, Memory: 45.2 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -5,25 +5,34 @@
 # 그러면, 각층의 맨 왼쪽부터 오른쪽은 행렬의 좌표로 치환할 수 있다
     # 1층 꼭대기는 [0,0]이고, 2층의 맨 왼쪽은 [1,0]인 셈
 
-def solution(n):
-    result = [[0 for _ in range(x)] for x in range(1,n+1)]
+# def solution(n):
+#     result = [[0 for _ in range(x)] for x in range(1,n+1)]
 
-    # rotation횟수는 = n (n이 4면 4번돈다 - 순서: 아래->오른쪽->왼쪽위)
-    x,y, value = -1,0, 0 # 좌표, 좌표에 들어갈 값
-    for i in range(n):
-        if i % 3 == 0:
-            for j in range(n-i):
-                x, value = x+1, value+1
-                result[x][y] = value
-        elif i % 3 == 1:
-            for j in range(n-i):
-                y, value = y+1, value+1
-                result[x][y] = value
-        else:
-            for j in range(n-i):
-                x,y, value = x-1,y-1, value+1
-                result[x][y] = value
+#     # rotation횟수는 = n (n이 4면 4번돈다 - 순서: 아래->오른쪽->왼쪽위)
+#     x,y, value = -1,0, 0 # 좌표, 좌표에 들어갈 값
+#     for i in range(n):
+#         if i % 3 == 0:
+#             for j in range(n-i):
+#                 x, value = x+1, value+1
+#                 result[x][y] = value
+#         elif i % 3 == 1:
+#             for j in range(n-i):
+#                 y, value = y+1, value+1
+#                 result[x][y] = value
+#         else:
+#             for j in range(n-i):
+#                 x,y, value = x-1,y-1, value+1
+#                 result[x][y] = value
     
-    return sum(result,[]) # 2차원 -> 1차원풀기
-
+#     return sum(result,[]) # 2차원 -> 1차원풀기
+def solution(n):
+    dx=[0,1,-1];dy=[1,0,-1]
+    b=[[0]*i for i in range(1,n+1)]
+    x,y=0,0;num=1;d=0
+    while num<=(n+1)*n//2:
+        b[y][x]=num
+        ny=y+dy[d];nx=x+dx[d];num+=1
+        if 0<=ny<n and 0<=nx<=ny and b[ny][nx]==0:y,x=ny,nx
+        else:d=(d+1)%3;y+=dy[d];x+=dx[d]
+    return sum(b,[])
 print(solution(5))
