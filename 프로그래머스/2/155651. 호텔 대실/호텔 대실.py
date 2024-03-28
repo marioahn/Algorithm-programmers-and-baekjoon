0e547c08735b46fe63c689500070876c99c808e4
marioahn <89332401+marioahn@users.noreply.github.com>
2024-03-28 12:18:03 +0900

    [level 2] Title: 호텔 대실, Time: 495.84 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -39,3 +39,18 @@ def solution(book_time):
 print(solution([["15:00", "17:00"], ["16:40", "18:20"], ["14:20", "15:20"], ["14:10", "19:20"], ["18:20", "21:20"]]))
 print(solution([["09:10", "10:10"], ["10:20", "12:20"]]))
 print(solution([["10:20", "12:30"], ["10:20", "12:30"], ["10:20", "12:30"]]))
+
+# 레퍼런스 코드 - 누적합 -> 훨---씬 빠름.
+    # 마지막 tc: 내 코드는 400ms인데, 이건 8ms
+# def solution(book_time):
+#     time_table = [0 for _ in range(60 * 24)]
+#     for start, end in book_time:
+#         start_minutes = 60 * int(start[:2]) + int(start[3:])
+#         end_minutes = 60 * int(end[:2]) + int(end[3:]) + 10
+
+#         if end_minutes > 60 * 24 - 1:
+#             end_minutes = 60 * 24 - 1
+
+#         for i in range(start_minutes, end_minutes):
+#             time_table[i] += 1
+#     return max(time_table)
