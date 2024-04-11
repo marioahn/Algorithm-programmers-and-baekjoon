89caf8f609422dd21643eba60a523255af9425f0
marioahn <89332401+marioahn@users.noreply.github.com>
2024-04-12 00:08:58 +0900

    [level 2] Title: N-Queen, Time: 1096.53 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -1,21 +1,26 @@
-def dfs(queen, n, row):
-    count = 0
-    
-    if n == row:
-        return 1
-    
-    for col in range(n):
-        queen[row] = col
-        for x in range(row):
-            if queen[x] == queen[row]: 
-                break
-            if abs(queen[x]-queen[row]) == (row-x):
-                break
-        else:
-            count += dfs(queen, n, row+1)
-            
-    return count
+ans = 0
+num = 0
+chkX = [False for i in range(32)]
+chkCross1 = [False for i in range(32)]
+chkCross2 = [False for i in range(32)]
+
+def nq(y, n):
+    global ans
+    x = 0
+    if y > n:
+        ans+=1
+    for x in range(1, n+1):
+        if chkX[x] or chkCross1[y + x] or chkCross2[(y - x) + n]:
+            continue
+        chkX[x] = True
+        chkCross1[y + x] = True
+        chkCross2[(y - x) + n] = True
+
+        nq(y + 1, n)
+        chkX[x] = False
+        chkCross1[y + x] = False
+        chkCross2[(y - x) + n] = False
 
 def solution(n):
-    queen = [0]*n        
-    return dfs(queen, n, 0)
+    nq(1, n)
+    return ans
