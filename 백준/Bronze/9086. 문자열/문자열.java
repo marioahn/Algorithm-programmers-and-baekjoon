e38e676e28f1d18cfe84a3831bdd273f4adfb423
marioahn <89332401+marioahn@users.noreply.github.com>
2024-04-23 15:19:33 +0900

    [Bronze V] Title: 문자열, Time: 152 ms, Memory: 16164 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,20 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        // 이렇게 문자열을 요소로 가진 배열 만듦!
         String name[] = new String[n];
-        for (int i=0; i<n; i++) {
-            // 그냥 문자는 next()하면 됨
-            String str = sc.next();
-            name[i] = str.substring(0,1)+str.substring(str.length()-1,str.length());
-        }
 
+        for (int i=0; i<n; i++) {
+            String str = br.readLine();
+            name[i] = str.charAt(0)
+                    + str.substring(str.length() - 1);
+        }
+        br.close();
         for (int i=0; i<n; i++) {
             System.out.println(name[i]);
+
         }
     }
 }
