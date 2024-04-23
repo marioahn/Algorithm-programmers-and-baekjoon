b204411109a5b3d580e95533b8d73eff8d86b4af
marioahn <89332401+marioahn@users.noreply.github.com>
2024-04-23 16:02:26 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13940 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\"); // 여기가 관건!!
+        System.out.println("|\"^\"`    |"); // 여기가 관건2
+        System.out.println("||_/=\\\\__|");
+    }
+}
