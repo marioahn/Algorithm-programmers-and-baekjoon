9f206151d0cd015070d078009e8dfdcbfb252349
marioahn <89332401+marioahn@users.noreply.github.com>
2024-04-23 15:57:06 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 14044 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("\\    /\\"); // \    /\ -> \문자에 \하나씩 더 추가해주면 됨
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
