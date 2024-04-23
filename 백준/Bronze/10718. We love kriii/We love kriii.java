fa605d7672c34f7102f5df08cd3e6d1330d35e7b
marioahn <89332401+marioahn@users.noreply.github.com>
2024-04-24 00:07:53 +0900

    [Bronze V] Title: We love kriii, Time: 120 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");     
+        
+        
+// 그 외의 방법들
+//		System.out.printf("%s","강한친구 대한육군\n강한친구 대한육군");
+//		System.out.printf("%s\n%s","강한친구 대한육군","강한친구 대한육군");
+//		System.out.printf("강한친구 대한육군\n강한친구 대한육군");
+ 
+	}
+}
