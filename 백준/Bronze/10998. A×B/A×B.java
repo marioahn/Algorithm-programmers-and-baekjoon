80a2172ddfcb611ab075b25c5c1a433139b3c820
marioahn <89332401+marioahn@users.noreply.github.com>
2024-04-27 10:47:08 +0900

    [Bronze V] Title: A×B, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/10998. A×B/A×B.java
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -1,15 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
  
 public class Main {
  
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
-		int A = in.nextInt();
-		int B = in.nextInt();
+	public static void main(String[] args) throws IOException {
+		     
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		System.out.println(A*B);
+		String[] str = br.readLine().split(" ");
+		int a = Integer.parseInt(str[0]);
+		int b = Integer.parseInt(str[1]);
+		
+		System.out.println(a*b);
  
-		in.close();
 	}
+ 
 }
