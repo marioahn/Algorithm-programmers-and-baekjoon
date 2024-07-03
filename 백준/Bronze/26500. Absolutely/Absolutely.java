e424e46feee89c52e0413f2992eea6135543b255
marioahn <89332401+marioahn@users.noreply.github.com>
2024-07-04 00:02:23 +0900

    [Bronze IV] Title: Absolutely, Time: 220 ms, Memory: 18132 KB -BaekjoonHub

---
+++ 백준/Bronze/26500. Absolutely/Absolutely.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		for(int i = 0; i < n; i++) {
+			double a = sc.nextDouble();
+			double b = sc.nextDouble();
+			System.out.println(String.format("%.1f", Math.abs(b - a)));
+		}
+		sc.close();
+	}
+}
