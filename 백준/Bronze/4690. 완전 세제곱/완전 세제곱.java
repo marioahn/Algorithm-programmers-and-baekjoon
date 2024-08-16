61e6f40402ba0efc28557484b3528eed4ed1efe0
marioahn <89332401+marioahn@users.noreply.github.com>
2024-08-16 10:30:18 +0900

    [Bronze III] Title: 완전 세제곱, Time: 168 ms, Memory: 16464 KB -BaekjoonHub

---
+++ 백준/Bronze/4690. 완전 세제곱/완전 세제곱.java
@@ -0,0 +1,19 @@
+public class Main {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		
+		for(int a = 2; a <= 100; a++) {
+			for(int b = 2; b <= a; b++) {
+				for(int c = b; c <= a; c++) {
+					for(int d = c; d <= a; d++) {
+						if(a*a*a == b*b*b + c*c*c + d*d*d) {
+							System.out.println("Cube = " + a + ", Triple = (" + b + "," + c + "," + d +")");
+						}
+					}
+				}
+			}
+		}
+	}
+
+}
