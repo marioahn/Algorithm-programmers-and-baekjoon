8e2052a0c0f908efa6c27a94069811bdbdcaeb5f
marioahn <89332401+marioahn@users.noreply.github.com>
2024-08-20 15:16:32 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/README.md
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 서울에서 김서방 찾기 - 12919 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12919?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12919) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.01 ms
+메모리: 10.1 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 23일 11:37:15
+2024년 08월 20일 15:16:26
 
 ### 문제 설명
 
--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,9 +1,18 @@
+# def solution(seoul):
+#     idx = 0
+#     # for i in range(0,len(seoul)):
+#     #     if seoul[i] == 'Kim':
+#     #         idx = i
+#     #         break
+#     idx = seoul.index('Kim')
+    
+#     return f'김서방은 {idx}에 있다'
+
 def solution(seoul):
     idx = 0
-    # for i in range(0,len(seoul)):
-    #     if seoul[i] == 'Kim':
-    #         idx = i
-    #         break
-    idx = seoul.index('Kim')
+    for i in range(0,len(seoul)):
+        if seoul[i] == 'Kim':
+            idx = i
+            break
     
     return f'김서방은 {idx}에 있다'
