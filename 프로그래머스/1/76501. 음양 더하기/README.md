fb43886d45040a51b754ef93ac4e478aba0bbd74
marioahn <89332401+marioahn@users.noreply.github.com>
2024-08-21 15:37:09 +0900

    [level 1] Title: 음양 더하기, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/README.md
+++ 프로그래머스/1/76501. 음양 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 음양 더하기 - 76501 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76501?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76501) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.11 ms
+메모리: 10.2 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 23일 11:53:45
+2024년 08월 21일 15:37:03
 
 ### 문제 설명
 
--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,15 +1,16 @@
-def solution(absolutes, signs):
-    result = 0
-    for i in range(len(absolutes)):
-        if signs[i] == False:
-            result += -(absolutes[i])
-        else:
-            result += absolutes[i]
+# def solution(absolutes, signs):
+#     result = 0
+#     for i in range(len(absolutes)):
+#         if signs[i] == False:
+#             result += -(absolutes[i])
+#         else:
+#             result += absolutes[i]
             
-    return result  
+#     return result  
 
 # 한 줄 코딩 생각해봤는데 잘 안되었음 -> 아래 참고 굿
 # for문, if문과 2개 변수를 동시에 사용하는 방법
 # return sum(absolutes if sign else -absolutes for absolutes, sign in zip(absolutes, signs))
-
+def solution(absoultes, signs):
+    return sum(absoultes if sign else - absoultes for absoultes, sign in zip(absoultes,signs))
 
