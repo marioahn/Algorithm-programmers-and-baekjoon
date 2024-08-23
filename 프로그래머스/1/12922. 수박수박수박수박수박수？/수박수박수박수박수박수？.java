7e165fb0aab5c9c2b6cc5244f5a2d2f149a5dd06
marioahn <89332401+marioahn@users.noreply.github.com>
2024-08-23 13:07:03 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.12 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,12 +1,6 @@
 class Solution {
     public String solution(int n) {
-        String answer = "";
-        
-        for (int i=1; i<=n; i++) {
-            if (i%2 != 0) answer += "수";
-            else answer += "박";
-        }
-        
-        return answer;
+        String answer = "수박".repeat(n);
+        return answer.substring(0,n);
     }
 }
