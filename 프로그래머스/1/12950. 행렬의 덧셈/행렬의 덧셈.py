3969149a3157dbab198de9645cb1deb9ba564089
marioahn <89332401+marioahn@users.noreply.github.com>
2024-08-24 17:49:19 +0900

    [level 1] Title: 행렬의 덧셈, Time: 23.35 ms, Memory: 23.1 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,7 +1,7 @@
 # 이렇게하면 여러 행렬을 동시에 더하는 코드로 수정하기도 쉬울 듯
 # '*'튜플 언팩킹을 활용하여, x라는 변수를 한개만 이용해서 두개의 튜플을 받아들여 처리
-def solution(A,B):
-    return [list(map(sum, zip(*x))) for x in zip(A, B)]
+def solution(arr1,arr2):
+    return [list(map(sum, zip(*x))) for x in zip(arr1, arr2)]
 
 # print(solution([[1,2], [2,3]], [[3,4],[5,6]]))
 # [[4, 6], [7, 9]]
