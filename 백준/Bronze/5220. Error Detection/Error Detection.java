506c8f3b405591fc38085caf79e89ed504cb39b8
marioahn <89332401+marioahn@users.noreply.github.com>
2024-08-25 17:51:00 +0900

    [Bronze III] Title: Error Detection, Time: 96 ms, Memory: 14016 KB -BaekjoonHub

---
+++ 백준/Bronze/5220. Error Detection/Error Detection.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static boolean solveErrorDetection(int n, int p) {
+        boolean valid = false;
+
+        int r = 0;
+
+        String binaryString = Integer.toBinaryString(n);
+
+        for(char c : binaryString.toCharArray()){
+            r += c -'0';
+        }
+
+        if((r%2==0 && p==0)||(r%2==1 && p==1)) {
+            valid = true;
+        }else {
+            valid = false;
+        }
+
+        return valid;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int numCases = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < numCases; i++)
+        {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int value = Integer.parseInt(st.nextToken());
+            int checkbit = Integer.parseInt(st.nextToken());
+
+            if (solveErrorDetection(value, checkbit)) {
+                System.out.println("Valid");
+            }
+            else {
+                System.out.println("Corrupt");
+            }
+        }
+    }
+}
