80f4548e2b569683c6c617595cb249d50f7a85ef
marioahn <89332401+marioahn@users.noreply.github.com>
2024-08-26 18:03:10 +0900

    [level 1] Title: 최소직사각형, Time: 1.78 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,18 +1,31 @@
+// class Solution {
+//     public int solution(int[][] sizes) {
+//         int max_row = 0; // 가로의 최대 길이
+//         int max_col = 0; // 세로
+        
+//         for (int i=0; i<sizes.length; i++) {
+//             if (sizes[i][0] < sizes[i][1]) {
+//                 int tmp = sizes[i][0];
+//                 sizes[i][0] = sizes[i][1];
+//                 sizes[i][1] = tmp;
+//             }
+//             if (max_row<sizes[i][0]) max_row = sizes[i][0];
+//             if (max_col<sizes[i][1]) max_col = sizes[i][1];
+//         }
+        
+//         return max_row*max_col;
+//     }
+// }
+
 class Solution {
     public int solution(int[][] sizes) {
-        int max_row = 0; // 가로의 최대 길이
-        int max_col = 0; // 세로
+        int length = 0, height = 0; // 아 이렇게 2개 나란히 쓸 수 있구나;
         
-        for (int i=0; i<sizes.length; i++) {
-            if (sizes[i][0] < sizes[i][1]) {
-                int tmp = sizes[i][0];
-                sizes[i][0] = sizes[i][1];
-                sizes[i][1] = tmp;
-            }
-            if (max_row<sizes[i][0]) max_row = sizes[i][0];
-            if (max_col<sizes[i][1]) max_col = sizes[i][1];
+        for (int[] card: sizes) {
+            length = Math.max(length, Math.max(card[0], card[1]));
+            height = Math.max(height, Math.min(card[0], card[1]));
         }
-        
-        return max_row*max_col;
+
+        return length * height;
     }
 }
