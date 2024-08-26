1b34a69a4362c33f4f977027ebc9eb59d8d7364a
marioahn <89332401+marioahn@users.noreply.github.com>
2024-08-26 12:25:06 +0900

    [level 1] Title: 행렬의 덧셈, Time: 34.44 ms, Memory: 23 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -3,7 +3,7 @@ def solution(arr1, arr2):
     
     for i in range(len(arr1)):
         arr_sum = []
-        for j in range(len(arr1[0])):
+        for j in range(len(arr1[i])):
             arr_sum.append(arr1[i][j] + arr2[i][j])
         answer.append(arr_sum)
     
