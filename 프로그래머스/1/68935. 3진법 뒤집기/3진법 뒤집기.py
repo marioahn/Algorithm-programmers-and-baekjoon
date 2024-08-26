6f12ab4464e9d771761f8697a0bb6576e6b9ab11
marioahn <89332401+marioahn@users.noreply.github.com>
2024-08-26 13:11:15 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,14 +1,26 @@
+# def solution(n):
+#     tmp = []
+#     while n>=3:
+#         tmp.append(n%3)
+#         n = n//3
+#     tmp.append(n)
+    
+#     tmp = tmp[::-1]
+    
+#     sum = 0
+#     for k in range(len(tmp)):
+#         sum += (3**k)*tmp[k]
+    
+#     return sum
+
 def solution(n):
-    tmp = []
-    while n>=3:
-        tmp.append(n%3)
+    tmp = ''
+    while n:
+        tmp += str(n%3)
         n = n//3
-    tmp.append(n)
     
-    tmp = tmp[::-1]
+    answer = int(tmp,3)
+    return answer
     
-    sum = 0
-    for k in range(len(tmp)):
-        sum += (3**k)*tmp[k]
     
-    return sum
+    
