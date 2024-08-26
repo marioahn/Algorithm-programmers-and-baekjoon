da84bd8a0167980062cf5e006265e69f866174ee
marioahn <89332401+marioahn@users.noreply.github.com>
2024-08-26 17:57:07 +0900

    [level 1] Title: 최소직사각형, Time: 6.04 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,7 +1,10 @@
-def solution(sizes):
-    max_list, min_list = [], []
-    for ele in sizes:
-        max_list.append(max(ele))
-        min_list.append(min(ele))
+# def solution(sizes):
+#     max_list, min_list = [], []
+#     for ele in sizes:
+#         max_list.append(max(ele))
+#         min_list.append(min(ele))
     
-    return max(max_list) * max(min_list)
+#     return max(max_list) * max(min_list)
+
+def solution(sizes):
+    return max(max(x) for x in sizes) * max(min(x) for x in sizes)
