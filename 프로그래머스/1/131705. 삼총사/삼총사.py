85bc486643bb0c09327674ce2499f2a1b72b04f5
marioahn <89332401+marioahn@users.noreply.github.com>
2024-08-26 14:07:28 +0900

    [level 1] Title: 삼총사, Time: 0.10 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,14 +1,9 @@
-# 걍 3중포문밖에 생각이 안 나는데
+from itertools import combinations
+
 def solution(number):
     cnt = 0
-    for i in range(len(number)-2):
-        for j in range(i+1, len(number)-1):
-            for k in range(j+1, len(number)):
-                if (number[i]+number[j]+number[k]) == 0:
-                    cnt +=1 
-
+    for tmp_list in combinations(number, 3):
+        if sum(tmp_list) == 0:
+            cnt += 1
+            
     return cnt
-
-
-print(solution([-2, 3, 0, 2, -5])) # 2
-print(solution([-3, -2, -1, 0, 1, 2, 3])) # 5
