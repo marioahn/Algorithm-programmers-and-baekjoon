e23388156b36d7d22b6506bdb3ff96dfd14bd3f8
marioahn <89332401+marioahn@users.noreply.github.com>
2024-08-26 14:01:42 +0900

    [level 1] Title: 삼총사, Time: 0.10 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -2,10 +2,10 @@
 def solution(number):
     cnt = 0
     for i in range(len(number)-2):
-        for j in range(i+1,len(number)-1):
-            for k in range(j+1,len(number)):
+        for j in range(i+1, len(number)-1):
+            for k in range(j+1, len(number)):
                 if (number[i]+number[j]+number[k]) == 0:
-                    cnt += 1
+                    cnt +=1 
 
     return cnt
 
