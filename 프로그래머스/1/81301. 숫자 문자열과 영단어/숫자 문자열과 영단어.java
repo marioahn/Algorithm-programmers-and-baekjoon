884b2b821f9276598c7ba4b3103923a4f13d57d1
marioahn <89332401+marioahn@users.noreply.github.com>
2024-09-02 13:12:54 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 2.34 ms, Memory: 68.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,11 +1,29 @@
+// class Solution {
+//     public int solution(String s) {
+//         String[] arr = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+        
+//         for (int i=0; i<arr.length; i++) {
+//             if (s.contains(arr[i])) {
+//                 s = s.replace(arr[i], Integer.toString(i));
+//             }
+//         }
+        
+//         return Integer.parseInt(s);
+//     }
+// }
+
+import java.util.*;
+
 class Solution {
     public int solution(String s) {
-        String[] arr = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+        int answer = 0;
+        int len = s.length();
+        StringBuilder sb = new StringBuilder("");
+        String[] digits = {"0","1","2","3","4","5","6","7","8","9"};
+        String[] alphabets = {"zero","one","two","three","four","five","six","seven","eight","nine"};
         
-        for (int i=0; i<arr.length; i++) {
-            if (s.contains(arr[i])) {
-                s = s.replace(arr[i], Integer.toString(i));
-            }
+        for (int i=0; i<10; i++) {
+            s = s.replaceAll(alphabets[i], digits[i]);
         }
         
         return Integer.parseInt(s);
