26d1bbf7b71a73b3c815a643ec37eeaa142b70b0
marioahn <89332401+marioahn@users.noreply.github.com>
2024-09-02 13:03:56 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -2,13 +2,12 @@ def solution(s):
     num = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
     
     for k in range(len(num)):
-        arr = s.split(num[k])
-        if len(arr) != 0:
-            s = str(k).join(arr)
+        s = s.replace(num[k], str(k))  # num[k]에 해당하는 영단어를 k로 대체
     
-    return int(s)
+    return int(s)  # 최종적으로 변환된 문자열을 정수로 변환하여 반환
 
 print(solution("one4seveneight"))
+
 
 # 레퍼런스 - wow;;;;;;;
 # num_dic = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
