f37a29b8803ed521f7b7231377c411105094f03a
marioahn <89332401+marioahn@users.noreply.github.com>
2024-09-06 12:00:35 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 3.55 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,31 +1,31 @@
 # 파이썬으로만 할 수 있는 깔끔한 방법이 있을것 같은데 흠
 # O(N**2)로 밖에 안되나 - 
 # dict사용하면 가능 - 및, stack.append(ele)는 그냥 if-else문 밖에 한줄만 써도 ok
-def solution(s):
-    stack = [] # idx찾을 용도(임시)
-    result = []
-
-    for ele in s:
-        if ele not in stack:
-            result.append(-1)
-            stack.append(ele)
-        else:
-            result.append(stack[::-1].index(ele)+1)
-            stack.append(ele)
-
-    return result
-
 # def solution(s):
-#     answer = []
-#     dic = dict()
-#     for i in range(len(s)):
-#         if s[i] not in dic:
-#             answer.append(-1)
-#         else:
-#             answer.append(i - dic[s[i]])
-#         dic[s[i]] = i
+#     stack = [] # idx찾을 용도(임시)
+#     result = []
 
-#     return answer
+#     for ele in s:
+#         if ele not in stack:
+#             result.append(-1)
+#             stack.append(ele)
+#         else:
+#             result.append(stack[::-1].index(ele)+1)
+#             stack.append(ele)
+
+#     return result
+
+def solution(s):
+    answer = []
+    dic = dict()
+    for i in range(len(s)):
+        if s[i] not in dic:
+            answer.append(-1)
+        else:
+            answer.append(i - dic[s[i]])
+        dic[s[i]] = i
+
+    return answer
 
 print(solution("banana")) # [-1, -1, -1, 2, 2, 2]
 print(solution("foobar")) # [-1, -1, 1, -1, -1, -1]
