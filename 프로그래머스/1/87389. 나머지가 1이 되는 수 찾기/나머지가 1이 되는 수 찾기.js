21dca8bb9f43172d0dd83f3ed46f9797a6b7380c
marioahn <89332401+marioahn@users.noreply.github.com>
2024-09-16 10:15:38 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 26.17 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -1,6 +1,14 @@
-function solution(n) {
-    let answer = 0;
-    for (let i=2; i<n; i++) {
-        if (n%i==1) return i
-    };
+// function solution(n) {
+//     for (let i=2; i<n; i++) {
+//         if (n%i==1) return i
+//     };
+// }
+
+// while: 조건식 안에서도 증감이 가능하네?
+function solution(n, x = 1) {    
+    while (x++) {
+        if (n % x === 1) {
+            return x;
+        }
+    }    
 }
